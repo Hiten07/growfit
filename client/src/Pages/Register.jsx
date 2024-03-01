1a6f1a07996ed83f7d5914b66555077be967c74d
rahulgohel20 <rahul.rahulgohel2091@gmail.com>
2024-03-01 20:27:38 +0530

    updated

--- client/src/Pages/Register.jsx
+++ client/src/Pages/Register.jsx
@@ -1,6 +1,23 @@
+import "./Registration.css";
+
 const Register = () => {
     return (
         <>
+        <section>
+            <main>
+                <div className="grid grid-cols-1 ">
+                    <div className="grid grid-cols-2 gap-10">
+                        <div className="registration-image">
+                            <img src="/images/registration.png" alt="Registration-image" width="500" height="500">
+                            </img>
+                        </div>
+                        <div className="registration-form">
+                            <h1 className="main-heading mb-3">Registration Form</h1>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </section>
         </>
     )
 }
