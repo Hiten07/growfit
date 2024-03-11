b5a13eff1c5ece695e799f797850bc34d80e5781
Hiten Thanki <hituthanki07@gmail.com>
2024-03-11 23:12:47 +0530

    stored registeration data

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
 
                         <div className="">
                             <div className="text-white">
-                                    <p class="p1">Connecting <h style={{color:"orange"}}>Your</h> Life <h style={{color:"orange"}}>With</h> Fitness !</p>
+                                    <p className="p1">Connecting <h style={{color:"orange"}}>Your</h> Life <h style={{color:"orange"}}>With</h> Fitness !</p>
                                     
                             </div>
                             <div className="">
@@ -47,16 +47,16 @@ const Home = () => {
                 <p className="about-us text-center my-5">About Us</p>
                 
             </div>
-            <div class="container d-flex align-items-center justify-content-center flex-wrap gap-5">
-                <div class="box">
-                    <div class="body">
-                        <div class="imgContainer">
+            <div className="container d-flex align-items-center justify-content-center flex-wrap gap-5">
+                <div className="box">
+                    <div className="body">
+                        <div className="imgContainer">
                             <img src="/images/find-gym.jpg" alt=""/>
                         </div>
-                <div class="content d-flex flex-column align-items-center justify-content-center">
+                <div className="content d-flex flex-column align-items-center justify-content-center">
                     <div>
-                        <h3 class=" fs-2 text-danger">Find Gym</h3>
-                        <p class="fs-4 text-white"><h className="fs-4">Find a gyms that fit for you</h><br/>-Know their address,services,
+                        <h3 className=" fs-2 text-danger">Find Gym</h3>
+                        <p className="fs-4 text-white"><h className="fs-4">Find a gyms that fit for you</h><br/>-Know their address,services,
                         charges and all you nedd a select a perfect for your killer workout place.</p>
                     </div>
                 </div>
@@ -64,31 +64,31 @@ const Home = () => {
             
         </div>
         
-        <div class="box">
-            <div class="body">
-                <div class="imgContainer">
+        <div className="box">
+            <div className="body">
+                <div className="imgContainer">
                     <img src="/images/workout-plan.jpg" alt=""/>
                 </div>
-                <div class="content d-flex flex-column align-items-center justify-content-center">
+                <div className="content d-flex flex-column align-items-center justify-content-center">
                     <div>
-                        <h3 class="text-danger fs-2">Workout Plan</h3>
-                        <p class="fs-4 text-white">Get your customized workout plan<br/>from your gym trainer from your profile.</p>
+                        <h3 className="text-danger fs-2">Workout Plan</h3>
+                        <p className="fs-4 text-white">Get your customized workout plan<br/>from your gym trainer from your profile.</p>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="box">
+        <div className="box">
             
-            <div class="body">
+            <div className="body">
                 
-                <div class="imgContainer">
+                <div className="imgContainer">
                     
                     <img src="/images/manage-data1.jpg" alt=""/>
                 </div>
-                <div class="content d-flex flex-column align-items-center justify-content-center">
+                <div className="content d-flex flex-column align-items-center justify-content-center">
                     <div>
-                        <h3 class="text-danger fs-2">Manage Data</h3>
-                        <p class="fs-4 text-white">Gym Owner can easily manage and update their customers data as well as membership fee in our system.</p>
+                        <h3 className="text-danger fs-2">Manage Data</h3>
+                        <p className="fs-4 text-white">Gym Owner can easily manage and update their customers data as well as membership fee in our system.</p>
                     </div>
                 </div>
             </div>
